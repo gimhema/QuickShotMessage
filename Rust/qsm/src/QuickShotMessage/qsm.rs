888df7e88a2c43c86e843507b5b1a30263231833
gimhema <49871739+gimhema@users.noreply.github.com>
2024-04-17 00:18:47 +0900

    Update qsm.rs
    .

--- Rust/qsm/src/QuickShotMessage/qsm.rs
+++ Rust/qsm/src/QuickShotMessage/qsm.rs
@@ -1,3 +1,81 @@
 use std::collections::VecDeque;
 
 
+
+pub enum QType {
+    DEFAULT,
+    QInt,
+    QFloat,
+    QString,
+    QArray,
+    QJson
+}
+
+pub struct QValue 
+{
+    qType : QType
+}
+
+impl QValue 
+{
+    pub fn new(_type : QType) -> QValue {
+        return QValue { qType: _type }
+    }
+
+    pub fn convert() -> String {
+        return "".to_string()
+    }
+}
+
+pub trait QAction {
+    fn Initialize(&mut self);
+    fn convert(&mut self) -> String;
+}
+
+pub struct QInteger 
+{
+    val : QValue,
+    data : i64
+}
+
+impl QAction for QInteger {
+    fn Initialize(&mut self) {
+        
+    }
+
+    fn convert(&mut self) -> String {
+        return "".to_string()
+    }
+}
+
+pub struct QFloat 
+{
+    val : QValue,
+    data : f64
+}
+
+impl QAction for QFloat {
+    fn Initialize(&mut self) {
+        
+    }
+
+    fn convert(&mut self) -> String {
+        return "".to_string()
+    }
+}
+
+pub struct QString 
+{
+    val : QValue,
+    data : String
+}
+
+impl QAction for QString {
+    fn Initialize(&mut self) {
+        
+    }
+
+    fn convert(&mut self) -> String {
+        return "".to_string()
+    }
+}
