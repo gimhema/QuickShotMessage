92cf4665195c8275d5cb661f8f0290ce9378c66e
gimhema <49871739+gimhema@users.noreply.github.com>
2024-05-03 02:08:00 +0000

    .

--- Rust/qsm/src/QuickShotMessage/example_msgs.rs
+++ Rust/qsm/src/QuickShotMessage/example_msgs.rs
@@ -47,6 +47,68 @@ impl Person {
     }
 }
 
+pub struct Actor {
+    id : i64,
+    loc_x : QFloat,
+    loc_y : QFloat,
+    loc_z : QFloat,
+    rot_x : QFloat,
+    rot_y : QFloat,
+    rot_z : QFloat,
+    rot_w : QFloat,
+}
+
+impl Actor {
+    pub fn new(
+        _id : i64,
+        _loc_x : QFloat,
+        _loc_y : QFloat,
+        _loc_z : QFloat,
+        _rot_x : QFloat,
+        _rot_y : QFloat,
+        _rot_z : QFloat,
+        _rot_w : QFloat, ) -> Self {
+        return Actor { id: _id, loc_x: _loc_x, loc_y: _loc_y, 
+            loc_z: _loc_z, rot_x: _rot_x, rot_y : _rot_y, rot_z : _rot_z, rot_w : _rot_w }
+    }
+}
+
+impl MessageBuilder for Actor
+{
+    fn message_build(mut self) -> QMessage {
+        let mut _data = Vec::new();
+        let mut _size = 0;
+
+        _data.push(self.loc_x.get_buffer());
+        _size += self.loc_x.get_buffer().len();
+
+        _data.push(self.loc_y.get_buffer());
+        _size += self.loc_y.get_buffer().len();
+
+        _data.push(self.loc_z.get_buffer());
+        _size += self.loc_z.get_buffer().len();
+
+        _data.push(self.rot_x.get_buffer());
+        _size += self.rot_x.get_buffer().len();
+
+        _data.push(self.rot_y.get_buffer());
+        _size += self.rot_y.get_buffer().len();
+
+        _data.push(self.rot_z.get_buffer());
+        _size += self.rot_z.get_buffer().len();
+
+        _data.push(self.rot_w.get_buffer());
+        _size += self.rot_w.get_buffer().len();
+
+
+        // size는 임시
+        let mut ret = QMessage::new(self.id,_size, _data);
+
+        return ret
+    }
+}
+
+
 pub fn TEST_Seriialize()
 {
 
@@ -76,6 +138,34 @@ QArray::new(vec![10, 32, 47], QType::QInt));
 
     let m_crate_elapsed_time = m_crate_end - m_crate_start;
     println!("message create time out: {:?}", m_crate_elapsed_time);
+
+
+    let start = Instant::now();
+
+    let mut actor = Actor::new(7, QFloat::new(3.1), QFloat::new(2.7)
+    , QFloat::new(11.6), QFloat::new(8.0), QFloat::new(9.1), QFloat::new(0.17)
+    , QFloat::new(8.665));
+
+    let mut actor_message = actor.message_build();
+
+    let end = Instant::now();
+    let elapsed_time = end - start;
+    println!("actor create time out: {:?}", elapsed_time);
+
+    println!("Actor Message Id : {}", actor_message.get_id().clone());
+    println!("Actor Message Size : {}", actor_message.get_size().clone());
+    println!("Actor Message Message : {:?}", actor_message.get_data());
+
+
+    let start = Instant::now();
+    
+    println!("Created Message : {}", seirialize(actor_message));
+
+    let end = Instant::now();
+    let elapsed_time = end - start;
+    println!("actor serialized time out: {:?}", elapsed_time);
+
+
 }
 
 pub fn TEST_Deseriialize()
