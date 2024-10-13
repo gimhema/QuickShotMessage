75177efa65f397d347ba089f644224f44acd4927
Kimmo <49871739+gimhema@users.noreply.github.com>
2024-10-13 13:20:54 +0900

    Update main.rs

--- v2/Rust/main.rs
+++ v2/Rust/main.rs
@@ -1,9 +1,11 @@
-
 mod qsm2;
 
 use qsm2::qsm::*;
 
-fn main() {
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
+fn main() -> std::io::Result<()>  {
     // 새로운 PackedData 인스턴스 생성
     // let data = PackedData::new(1, 123456789);
     
@@ -21,12 +23,35 @@ fn main() {
     //     }
     // }
 
-    let invalid_message = BaseMessage::new(0).serialize();
-    handle_message(&invalid_message);
+    // let invalid_message = BaseMessage::new(0).serialize();
+    // handle_message(&invalid_message);
 
-    // id가 1인 PackedData 메시지 생성
-    let packed_data = PackedData::new(1, 123456789);
-    let packed_data_serialized = packed_data.serialize();
-    handle_message(&packed_data_serialized);
+    // // id가 1인 PackedData 메시지 생성
+    // let packed_data = PackedData::new(1, 123456789);
+    // let packed_data_serialized = packed_data.serialize();
+    // handle_message(&packed_data_serialized);
+
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    println!("Server listening on port 8080");
+
+    for stream in listener.incoming() {
+        let mut stream = stream?;
+        let mut buffer = vec![0; std::mem::size_of::<PackedData>()]; // PackedData 크기 만큼의 버퍼 생성
+        
+        // 데이터가 충분히 수신될 때까지 읽기
+        let mut total_read = 0;
+        while total_read < buffer.len() {
+            let bytes_read = stream.read(&mut buffer[total_read..])?;
+            if bytes_read == 0 {
+                // 연결이 종료된 경우
+                break;
+            }
+            total_read += bytes_read;
+        }
+
+        handle_message(&buffer);
+    }
+
+    Ok(())
 
 }
