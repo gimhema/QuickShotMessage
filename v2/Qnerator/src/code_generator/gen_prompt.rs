6abae66d21bcc01696c661e6ca1464fd1dbee831
kimmo <hemakim0215@naver.com>
2024-11-02 11:54:44 +0900

    .

--- v2/Qnerator/src/code_generator/gen_prompt.rs
+++ v2/Qnerator/src/code_generator/gen_prompt.rs
@@ -2,12 +2,36 @@
 
 use super::gen_trait::*;
 
+// qnerator -f <FILE_NAME> <GENERATE_DIRECTORY>
+// qnerator -d <FILE_DIRECTORY> <GENERATE_DIRECTORY>
+
+pub enum MODE {
+    DEFAULT,
+    FILE,
+    DIRECTORY
+}
+
 pub struct GenPrompt {
-    property: CodeGenProperty
+    property: CodeGenProperty,
+    mode : MODE
 }
 
 impl GenPrompt {
     pub fn new() -> Self {
-        return GenPrompt{property : CodeGenProperty::new()}
+        return GenPrompt{property : CodeGenProperty::new(), mode : MODE::DEFAULT}
+    }
+
+    fn get_first_word<'a>(&mut self, s: &'a str) -> &'a str {
+        s.split_whitespace().next().unwrap_or("")
+    }
+
+    pub fn read_argv(&mut self, argv: String) -> MODE {
+        let first_word = self.get_first_word(&argv);
+
+        match first_word.to_lowercase().as_str() {
+            "-f" => MODE::FILE,
+            "-d" => MODE::DIRECTORY,
+            _ => MODE::DEFAULT,
+        }
     }
 }
