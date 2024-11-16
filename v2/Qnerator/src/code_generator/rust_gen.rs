cc5dce2884ab01d2be37f2492cf11430c8f83975
kimmo <hemakim0215@naver.com>
2024-11-16 15:54:39 +0900

    .

--- v2/Qnerator/src/code_generator/rust_gen.rs
+++ v2/Qnerator/src/code_generator/rust_gen.rs
@@ -14,6 +14,18 @@ impl RustGenerator {
     pub fn new() -> Self {
         return RustGenerator{source : "".to_string()}
     }
+
+    pub fn format_rust_code(&mut self, struct_name: &str, fields: &[((String, String))]) -> String {
+        let mut rust_code = String::new();
+
+        return rust_code
+    }
+
+    pub fn generate_rust_code(&mut self, contents: &str) -> Option<String> {
+        let rust_code = String::new();
+
+        return Some(rust_code)
+    }
 }
 
 
