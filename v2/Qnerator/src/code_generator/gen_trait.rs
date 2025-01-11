389674398ebbae133e1deb72ffa26945de79baa1
kimmo <hemakim0215@naver.com>
2025-01-11 10:38:12 +0900

    .

--- v2/Qnerator/src/code_generator/gen_trait.rs
+++ v2/Qnerator/src/code_generator/gen_trait.rs
@@ -5,65 +5,7 @@ use std::io::{BufRead, BufReader};
 use std::io::{self, Write};
 use std::path::Path;
 
-#[derive(Clone)]
-pub enum GenType {
-    NONE,
-    CPP,
-    RUST,
-    GO,
-    PYTHON,
-    CSHARP
-}
 
-#[derive(Clone)]
-pub struct CodeGenProperty {
-    target_file_directory: String,
-    generate_directory: String,
-    file_name: String,
-    gen_mode: GenType
-}
-
-impl CodeGenProperty {
-    pub fn new() -> Self {
-        return CodeGenProperty{
-            target_file_directory : "".to_string(),
-            generate_directory : "".to_string(),
-             file_name : "".to_string(),
-              gen_mode : GenType::NONE}
-    }
-
-    pub fn get_generate_directory(&mut self) -> String {
-        return self.generate_directory.clone()
-    }
-
-    pub fn get_file_name(&mut self) -> String {
-        return self.file_name.clone()
-    }
-
-    pub fn get_mode(&mut self) -> GenType {
-        return self.gen_mode.clone()
-    }
-
-    pub fn get_target_file_directory(&mut self) -> String {
-        return self.target_file_directory.clone()
-    }
-
-    pub fn set_target_file_directory(&mut self, _directory: String) {
-        self.target_file_directory = _directory;
-    }
-
-    pub fn set_generate_directory(&mut self, _direcotry : String) {
-        self.generate_directory = _direcotry;
-    }
-
-    pub fn set_file_name(&mut self, _file_name : String) {
-        self.file_name = _file_name;
-    }
-
-    pub fn set_mode(&mut self, _type : GenType) {
-        self.gen_mode = _type;
-    }
-}
 
 
 pub fn read_parse_struct(direcotry_name : String, file_name : String) -> Vec<(String, String)> {
--- v2/Qnerator/src/code_generator/mod.rs
+++ v2/Qnerator/src/code_generator/mod.rs
@@ -4,6 +4,7 @@ pub mod python_gen;
 pub mod rust_gen;
 pub mod gen_trait;
 pub mod gen_prompt;
+pub mod code_gen_option;
 
 use gen_trait::*;
 use cpp_gen::*;
@@ -11,6 +12,7 @@ use go_gen::*;
 use python_gen::*;
 use rust_gen::*;
 use gen_prompt::*;
+use code_gen_option::*;
 
 
 pub mod code_generator {
